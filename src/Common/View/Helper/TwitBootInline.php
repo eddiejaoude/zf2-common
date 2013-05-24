2cc1d5cae57cb0e692a81b6b8b2499f285f4457b
Eddie Jaoude <eddie@jaoudestudios.com>
2013-05-24 20:23:28 +0100

    Initial commit of form common helper

---
+++ src/Common/View/Helper/TwitBootInline.php
@@ -0,0 +1,65 @@
+<?php
+namespace Job\View\Helper;
+
+use Zend\Stdlib\DateTime;
+use Zend\View\Helper\AbstractHelper;
+use Job\Form\ItemForm;
+
+class Form extends AbstractHelper
+{
+
+    /**
+     * Required for view helper
+     *
+     * @return Form
+     */
+    public function __invoke()
+    {
+        return $this;
+    }
+
+    /**
+     * Wraps original call with twitter bootstrap form helper
+     *
+     * @param string $method
+     * @param array $args
+     * @return string
+     */
+    public function __call($method, $args)
+    {
+        $field = $args[0];
+
+        $view = '<div class="control-group' . $this->isError($field) . '">';
+        $view .= '<label class="control-label" for="' . $field->getAttribute('name') . '">' . $field->getOption('label') . '</label>';
+        $view .= '<div class="controls">';
+        $view .= $this->getView()->{$method}($field);
+        $view .= $this->getView()->formElementErrors()
+            ->setMessageOpenFormat('<span class="help-inline">')
+            ->setMessageSeparatorString(', ')
+            ->setMessageCloseString('</span>')
+            ->render($field);
+        $view .= '</div>';
+        $view .= '</div>';
+
+        return $view;
+    }
+
+    /**
+     * Checks for field error
+     *
+     * @param Form $field
+     * @return string
+     */
+    public function isError($field)
+    {
+        $error = $this->getView()->formElementErrors()->render($field);
+        if (!empty($error)) {
+            return ' error';
+        }
+
+        return '';
+    }
+
+
+
+}
