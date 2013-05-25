a9706ddc476fb1174c17a97dbeeca21874c8e3fd
Eddie Jaoude <eddie@jaoudestudios.com>
2013-05-25 07:36:42 +0100

    Updated composer

--- config/module.config.php
+++ config/module.config.php
@@ -2,7 +2,7 @@
 return array(
     'view_helpers' => array(
         'invokables' => array(
-            'formHelper' => 'Common\View\Helper\Form',
+            'formHelper' => 'Common\View\Helper\TwitBootInline',
         ),
     ),
 
