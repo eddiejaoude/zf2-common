89392ed911c4dc40ced05c463f8414db2a2c916a
Eddie Jaoude <eddie@jaoudestudios.com>
2013-05-25 07:47:01 +0100

    Added messages helper

--- config/module.config.php
+++ config/module.config.php
@@ -3,6 +3,7 @@ return array(
     'view_helpers' => array(
         'invokables' => array(
             'formHelper' => 'Common\View\Helper\TwitBootInline',
+            'messages'   => 'Common\View\Helper\TwitBootMessages',
         ),
     ),
 
