1cd8958ed29c6a174a9b79b9ad680362fbc3138b
Eddie Jaoude <eddie@jaoudestudios.com>
2013-07-18 06:23:26 +0100

    Added string shortener to view helpers

---
+++ src/Common/View/Helper/ShortenText.php
@@ -0,0 +1,33 @@
+<?php
+namespace Common\View\Helper;
+
+use Zend\Stdlib\DateTime;
+use Zend\View\Helper\AbstractHelper;
+
+/**
+ * Class ShortenText
+ * @package Common\View\Helper
+ */
+class ShortenText extends AbstractHelper
+{
+
+    /**
+     * Shorten text to a selected size
+     *
+     * @param string $text
+     * @param int    $length
+     * @param string $divider
+     * @return string
+     */
+    public function __invoke($text, $length = 60, $divider = '...')
+    {
+        if (strlen($text) < $length) {
+            return $text;
+        }
+
+        $halfPoint = $length / 2;
+        $shorterText = substr($text, 0, $halfPoint) . '...' . substr($text, -$halfPoint, strlen($text));
+
+        return $shorterText;
+    }
+}
