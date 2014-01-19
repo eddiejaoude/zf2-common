fa7212f473ab69486f97e25b4a853e1ca1c774a3
Eddie Jaoude <eddie@jaoudestudios.com>
2014-01-19 10:27:11 +0000

    Updated TwitBootMessages for TwitterBootstrap v3

--- src/Common/View/Helper/TwitBootMessages.php
+++ src/Common/View/Helper/TwitBootMessages.php
@@ -6,7 +6,7 @@ use Zend\View\Helper\AbstractHelper;
 use Zend\View\Helper\FlashMessenger;
 
 /**
- * Class Messsages
+ * Class Messages
  *
  * @package Common\View\Helper
  */
@@ -14,60 +14,15 @@ class TwitBootMessages extends AbstractHelper
 {
 
     /**
-     * Message types
-     *
-     * @var array
-     */
-    protected $types = array('', 'success', 'error', 'info');
-
-    protected $icons = array(
-        'icon-info-sign',
-        'success' => 'icon-ok-sign',
-        'error'   => 'icon-minus-sign'
-    );
-
-    /**
-     * Format date to user friendly
-     *
      * @param FlashMessenger $flashMessenger
      * @return string
      */
     public function __invoke(FlashMessenger $flashMessenger)
     {
-        $output = '';
-        foreach ($this->types as $type) {
-            foreach ($flashMessenger->{'get' . ucfirst($type) . 'Messages'}() as $message) {
-                $output .= $this->getAlert($type, $message);
-            }
-        }
+        $flashMessenger->setMessageOpenFormat('<div%s><ul><li>')
+            ->setMessageSeparatorString('</li><li>')
+            ->setMessageCloseString('</li></ul></div>');
 
-        return $output;
+        return $flashMessenger;
     }
-
-    /**
-     * @param string $type
-     * @param string $message
-     * @return string
-     */
-    public function getAlert($type, $message)
-    {
-        $output = '
-            <div class="alert alert-' . $type . '">
-                <i class="' . $this->getIcon($type) . ' icon-white"></i>
-                ' . $message . '
-            </div>
-            ';
-
-        return $output;
-    }
-
-    /**
-     * @param string $type
-     * @return string
-     */
-    public function getIcon($type)
-    {
-        return empty($this->icons[$type]) ? $this->icons[0] : $this->icons[$type];
-    }
-
 }
